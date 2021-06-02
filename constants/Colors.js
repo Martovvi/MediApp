df493a82d816f1191aef36aa207b92322a94341d
Martin <55866297+Martovvi@users.noreply.github.com>
2021-06-02 17:48:02 +0200

    Created folders screens, constants and restructured the code for cleanliness

---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+export default {
+  primary: "#bf2e2e",
+  accent: "#c3ea9b",
+  lightBackground: "#f4f4f4",
+};
---
+++ screens/MainScreen.js
@@ -0,0 +1,88 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import Colors from "../constants/Colors";
+import LayoutStyles from "../constants/LayoutStyles";
+import React from "react";
+
+export default MainScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.topContainer, LayoutStyles.topContainer]}>
+        <Text style={styles.appTitle}> Medi App </Text>
+      </View>
+      <View style={[styles.middleContainer, LayoutStyles.middleContainer]}>
+        <View style={styles.middleElement}>
+          <View style={styles.symbolElement}>
+            <Text style={styles.symbolText}> Sym</Text>
+          </View>
+          <Text style={styles.middleText}> BMI Rechner </Text>
+        </View>
+      </View>
+      <View style={[styles.bottomContainer, LayoutStyles.bottomContainer]}>
+        <Text style={styles.bottomText}> + </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topContainer: {
+    backgroundColor: Colors.primary,
+  },
+  appTitle: {
+    fontSize: 45,
+    color: "white",
+    fontWeight: "bold",
+  },
+  middleContainer: {
+    backgroundColor: Colors.lightBackground,
+  },
+  middleElement: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: Colors.accent,
+    padding: 10,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    borderColor: "black",
+    borderWidth: 2,
+    width: "90%",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+  symbolElement: {
+    backgroundColor: "red",
+    padding: 15,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    backgroundColor: Colors.lightBackground,
+    borderColor: "black",
+    borderWidth: 2,
+  },
+  symbolText: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  middleText: {
+    fontSize: 25,
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+  bottomContainer: {
+    backgroundColor: Colors.primary,
+  },
+  bottomText: {
+    fontSize: 75,
+    marginBottom: 10,
+    color: "white",
+  },
+});
