a4665353ee36c421ed0d54511c7f4e7838dd1e6b
Martin <55866297+Martovvi@users.noreply.github.com>
2021-06-09 15:40:57 +0200

    ModulListScreen hinzugefügt

--- screens/ModulListScreen.js
+++ screens/ModulListScreen.js
@@ -1,10 +1,67 @@
-import React from 'react'
-import {View, Text} from 'react-native'
+import { StyleSheet, Text, View } from "react-native";
 
-export default ModulListScreen = props => {
-    return(
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>ModulListScreen</Text>
-        </View>
-    );
+import BgButton from "../components/BgButton";
+import Colors from "../constants/Colors";
+import LayoutStyles from "../constants/LayoutStyles";
+import ModulList from "../components/ModulList";
+import React from "react";
+
+export default ModulListScreen = (props) => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.topContainer, LayoutStyles.topContainer]}>
+        <Text style={styles.appTitle}> Module </Text>
+      </View>
+      <View
+        style={[styles.middleContainer, LayoutStyles.middleContainer]}
+      ></View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topContainer: {
+    backgroundColor: Colors.primary,
+  },
+  appTitle: {
+    fontSize: 45,
+    color: "white",
+    fontWeight: "bold",
+  },
+  middleContainer: {
+    backgroundColor: Colors.lightBackground,
+  },
+  middleElement: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: Colors.accent,
+    padding: 10,
+    borderRadius: 15,
+    borderColor: "black",
+    borderWidth: 2,
+    width: "90%",
+    marginTop: 15,
+    marginBottom: 10,
+  },
+  symbolElement: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: Colors.lightBackground,
+    borderColor: "black",
+    borderWidth: 2,
+  },
+  symbolText: {
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  middleText: {
+    fontSize: 25,
+    marginTop: 10,
+    marginBottom: 10,
+    fontWeight: "bold",
+  },
+});
